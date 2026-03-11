d053a56400bfe77d60805668d7be7ea52465c784
Everton Portela Santos <everton.portela@nstech.com.br>
2026-03-11 11:22:41 -0300

    chore: Initial project setup for projLogicaProgramacao
    - Added NetBeans project configuration files: genfiles.properties, project.properties, and project.xml.
    - Created main application class ProjLogicaProgramacao with a simple ArrayList example.

---
+++ projLogicaProgramacao/src/projlogicaprogramacao/ProjLogicaProgramacao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projlogicaprogramacao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author everton.santos
+ */
+public class ProjLogicaProgramacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        ArrayList<Integer> lista = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            lista.add(i, i + 1);
+        }
+        System.out.println(lista.get(4));
+    }
+
+}
